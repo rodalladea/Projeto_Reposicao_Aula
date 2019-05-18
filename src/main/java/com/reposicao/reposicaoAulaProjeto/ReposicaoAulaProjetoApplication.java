76a0bea5b4c2f5c4d8943361c9e53b6507857787
ErikZambeli <erik_12_nf@live.com>
2019-05-18 01:38:31 -0300

    Iniciando projeto

---
+++ src/main/java/com/reposicao/reposicaoAulaProjeto/ReposicaoAulaProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.reposicao.reposicaoAulaProjeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposicaoAulaProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposicaoAulaProjetoApplication.class, args);
+	}
+
+}
